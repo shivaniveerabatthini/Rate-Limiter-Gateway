36416aeb1fed1a401bd69ab1d6a36cb5cf5091ff
shivaniveerabatthini <veerabatthinishivani20@gmail.com>
2026-07-13 22:38:48 +0530

    Configure gateway for render

--- gateway/src/main/java/com/example/gateway/client/ProductClient.java
+++ gateway/src/main/java/com/example/gateway/client/ProductClient.java
@@ -1,24 +1,24 @@
 package com.example.gateway.client;
 
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
-import io.github.resilience4j.retry.annotation.Retry;
-import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 
 @Component
 public class ProductClient {
 
     private final RestTemplate restTemplate = new RestTemplate();
 
+    private static final String PRODUCT_SERVICE_URL =
+            "https://product-service-bd11.onrender.com/products";
+
     @Retry(name = "productService")
     @CircuitBreaker(name = "productService", fallbackMethod = "fallback")
     @Bulkhead(name = "productService", fallbackMethod = "fallback")
     public String getProducts() {
-        return restTemplate.getForObject(
-                "http://product-service:9090/products",
-                String.class
-        );
+        return restTemplate.getForObject(PRODUCT_SERVICE_URL, String.class);
     }
 
     public String fallback(Throwable t) {
